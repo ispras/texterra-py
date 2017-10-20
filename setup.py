4f092831f958234167f88711441f37ff36ab671b
tsolakghukasyan <tsggukasyan@edu.hse.ru>
2017-10-20 15:00:14 +0300

    minor setup.py update

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 import ez_setup
-
 ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 
@@ -7,10 +6,9 @@ setup(
     name="texterra",
     desciption="API for natural language processing.",
     version='1.0.0',
-    url="https://api.ispras.ru/",
-    long_description="""Texterra API provides tools for natural language processing, \
-    social networks analysis, and knowledge base utilization.""",
-    license="",
+    url="https://texterra.ispras.ru",
+    long_description="""Texterra API provides tools for natural language processing and knowledge base utilization.""",
+    license="Apache License, Version 2.0",
     keywords=['natural language processing', 'text processing', 'parsing', 'tagging', 'tokenizing', 'syntax',
               'language', 'lemmatization', 'named entity recognition', 'spelling correction', 'sentiment analysis',
               'disambiguation', 'key concepts detection'],
