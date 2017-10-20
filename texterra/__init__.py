b40284a598ff63d6a285d0e9ec56a972d120f59d
tsolakghukasyan <tsggukasyan@edu.hse.ru>
2017-10-20 14:47:54 +0300

    add description in init, specify version

--- texterra/__init__.py
+++ texterra/__init__.py
@@ -1,2 +1,10 @@
+"""
+Texterra API is a Python module providing tools for natural language processing and
+knowledge base utilization.
+
+See https://texterra.ispras.ru for more information about Texterra project.
+"""
 from .texterra import API
 from .syntaxTree import SyntaxTree
+
+__version__ = '1.0.0'
