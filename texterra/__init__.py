d7792f788e69b47aca0dad58fece0db5b9b754e4
tsolakghukasyan <tsggukasyan@edu.hse.ru>
2017-10-23 18:31:10 +0300

    improve package description in __init__.py

--- texterra/__init__.py
+++ texterra/__init__.py
@@ -1,6 +1,7 @@
 """
-Texterra API is a Python module providing tools for natural language processing and
-knowledge base utilization.
+This package provides a Python interface for Texterra API.
+Texterra is a toolkit for natural language processing and knowledge base utilization,
+developed by a team of researchers at the Ivannikov Institute for System Programming.
 
 See https://texterra.ispras.ru for more information about Texterra project.
 """
