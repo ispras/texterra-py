ec5a94e7434cd61781df018a2694b42f17e71ef5
tsolakghukasyan <tsggukasyan@edu.hse.ru>
2017-10-26 16:43:48 +0300

    indicate supported natural languages

--- texterra/__init__.py
+++ texterra/__init__.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 """
+Texterra Python Interface
+=========================
+
 This package provides a Python interface for Texterra API.
 Texterra is a toolkit for natural language processing and knowledge base utilization,
 developed by a team of researchers at the Ivannikov Institute for System Programming.
+Currently, English and Russian languages are supported.
 
 See https://texterra.ispras.ru for more information about Texterra project.
 """
